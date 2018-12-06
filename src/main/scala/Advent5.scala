9878c08d07050cfed80b1d31584a440bc673edb7
Christopher Cerny <cerny.christopher@gmx.at>
2018-12-06 21:24:48 +0100

    5.1

---
+++ src/main/scala/Advent5.scala
@@ -0,0 +1,34 @@
+import scala.io.Source
+
+object Advent5 {
+  /*VERY SLOW:
+  def react(s: String): String = {
+    @scala.annotation.tailrec
+    def combine(soFar: List[Char], next: List[Char]): List[Char] =
+      next match {
+        case a :: b :: tail if a.isLower && b.isUpper && b.toLower == a => combine(soFar, tail)
+        case a :: b :: tail if a.isUpper && b.isLower && b.toUpper == a => combine(soFar, tail)
+        case a :: b :: tail => combine(soFar ++ List(a), b :: tail)
+        case x => soFar ++ next
+      }
+
+    combine(Nil, s.toList).mkString
+  }*/
+
+  private def subReact(s:String): String = if (s.isEmpty) "" else {
+    s.grouped(2).filterNot(group => group.length == 2 && group(0).toLower == group(1).toLower && group(0).isLower != group(1).isLower).mkString
+  }
+
+  private def react(s:String): String = {
+    val even = subReact(s)
+    even.take(1) ++ subReact(even.drop(1))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val testInput: String = Source.fromURL(getClass.getResource("/5.input")).getLines().next()
+    val reducedInputs = Stream.iterate(testInput)(react)
+    val reducedInputLengths = reducedInputs.map(_.length)
+    val solution1 = reducedInputLengths.zip(reducedInputLengths.drop(1)).dropWhile(pair => pair._1 != pair._2).head._1
+    println(solution1)
+  }
+}
